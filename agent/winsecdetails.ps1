3edd8be3315b8e83ece4bf67e677c56388662c6c
Fish-dev2 <capn.fuzi.balint@gmail.com>
2022-06-30 16:53:40 +0200

    Finish winsecdetailsps1
    Finished winsecdetails to create the xml command

--- agent/winsecdetails.ps1
+++ agent/winsecdetails.ps1
@@ -1,1 +1,34 @@
-Write-Output "asd"
+
+$details = Get-MpComputerStatus
+
+$xml = ""
+$xml += "<WINSECDETAILS>`n"
+$xml += "<AMENGINEVERSION>" + $details.AMEngineVersion + "</AMENGINEVERSION>"
+$xml += "<AMPRODUCTVERSION>" + $details.AMProductVersion + "</AMPRODUCTVERSION>"
+$xml += "<AMRUNNINGMODE>" + $details.AMRunningMode + "</AMRUNNINGMODE>"
+$xml += "<AMSERVICEENABLED>" + $details.AMServiceEnabled + "</AMSERVICEENABLED>"
+$xml += "<AMSERVICEVERSION>" + $details.AMServiceVersion + "</AMSERVICEVERSION>"
+$xml += "<ANTISPYWAREENABLED>" + $details.AntispywareEnabled + "</ANTISPYWAREENABLED>"
+$xml += "<ANTISPYWARESIGNATUREAGE>" + $details.AntispywareSignatureAge + "</ANTISPYWARESIGNATUREAGE>"
+$xml += "<ANTISPYWARESIGNATURELASTUPDATED>" + $details.AntispywareSignatureLastUpdated + "</ANTISPYWARESIGNATURELASTUPDATED>"
+$xml += "<ANTISPYWARESIGNATUREVERSION>" + $details.AntispywareSignatureVersion + "</ANTISPYWARESIGNATUREVERSION>"
+$xml += "<ANTIVIRUSENABLED>" + $details.AntivirusEnabled + "</ANTIVIRUSENABLED>"
+$xml += "<ANTIVIRUSSIGNATUREAGE>" + $details.AntivirusSignatureAge + "</ANTIVIRUSSIGNATUREAGE>"
+$xml += "<ANTIVIRUSSIGNATURELASTUPDATED>" + $details.AntivirusSignatureLastUpdated + "</ANTIVIRUSSIGNATURELASTUPDATED>"      
+$xml += "<ANTIVIRUSSIGNATUREVERSION>" + $details.AntivirusSignatureVersion + "</ANTIVIRUSSIGNATUREVERSION>"
+$xml += "<BEHAVIORMONITORENABLED>" + $details.BehaviorMonitorEnabled + "</BEHAVIORMONITORENABLED>"
+$xml += "<IOAVPROTECTIONENABLED>" + $details.IoavProtectionEnabled + "</IOAVPROTECTIONENABLED>"
+$xml += "<ISTAMPERPROTECTED>" + $details.IsTamperProtected + "</ISTAMPERPROTECTED>"
+$xml += "<NISENABLED>" + $details.NISEnabled + "</NISENABLED>"
+$xml += "<NISENGINEVERSION>" + $details.NISEngineVersion + "</NISENGINEVERSION>"
+$xml += "<NISSIGNATUREAGE>" + $details.NISSignatureAge + "</NISSIGNATUREAGE>"
+$xml += "<NISSIGNATURELASTUPDATED>" + $details.NISSignatureLastUpdated + "</NISSIGNATURELASTUPDATED>"
+$xml += "<NISSIGNATUREVERSION>" + $details.NISSignatureVersion + "</NISSIGNATUREVERSION>"
+$xml += "<ONACCESSPROTECTIONENABLED>" + $details.OnAccessProtectionEnabled + "</ONACCESSPROTECTIONENABLED>"
+$xml += "<REALTIMEPROTECTIONENABLED>" + $details.RealTimeProtectionEnabled + "</REALTIMEPROTECTIONENABLED>"
+$xml += "<TAMPERPROTECTIONSOURCE>" + $details.TamperProtectionSource + "</TAMPERPROTECTIONSOURCE>"
+$xml += "</WINSECDETAILS>`n"
+
+
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+[Console]::WriteLine($xml)
