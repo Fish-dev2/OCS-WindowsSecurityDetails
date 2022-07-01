810b1c9de04a098f631952e1c0ba8705178b4286
Fish-dev2 <capn.fuzi.balint@gmail.com>
2022-07-01 13:23:33 +0200

    Small changes to fit with other examples

--- cd_winsecdetails/cd_winsecdetails.php
+++ cd_winsecdetails/cd_winsecdetails.php
@@ -1,19 +1,13 @@
 <?php
 ###############################################################################
 ## OCSINVENTORY-NG
-## Copyleft Léa DROGUET 2020
+## Copyleft Bálint FÜZI 2022
 ## Web : http://www.ocsinventory-ng.org
 ##
 ## This code is open source and may be copied and modified as long as the source
 ## code is always made freely available.
 ## Please refer to the General Public Licence http://www.gnu.org/ or Licence.txt
 ################################################################################
-
-
- /**
-  * This file is used to build a table refering to the plugin and define its 
-  * default columns as well as SQL request.
-  */
 
 if (AJAX) {
     parse_str($protectedPost['ocs']['0'], $params);
@@ -23,50 +17,19 @@ if (AJAX) {
 } else {
     $ajax = false;
 }
-## DONT CHANGE THIS ^^^^^^
 
-
-
-
-// print a title for the table
 print_item_header("Windows Security Details");
 
-
-
-
-
-
-
-
-######NOCHANGEVVVV
 if (!isset($protectedPost['SHOW'])) {
     $protectedPost['SHOW'] = 'NOSHOW';
 }
-######^^^^^^
 
-
-
-
-
-
-// form details and tab options
 $form_name = "winsecdetails";^
 
-
-
-
-
-#########VVVVVV
 $table_name = $form_name;
 $tab_options = $protectedPost;
 $tab_options['form_name'] = $form_name;
 $tab_options['table_name'] = $table_name;
-####NOCHANGE^^^^
-
-
-
-
-
 
 echo open_form($form_name);
 $list_fields = array(
@@ -94,19 +57,10 @@ $list_fields = array(
                     'OnAccessProtectionEnabled' => 'OnAccessProtectionEnabled',
                     'RealTimeProtectionEnabled' => 'RealTimeProtectionEnabled',
                     'TamperProtectionSource' => 'TamperProtectionSource'
-                );
-// columns to include at any time and default columns
-
-
-
-
-
-
-#NOCHANGE VVVVVVVV
+);
 $list_col_cant_del = $list_fields;
 $default_fields = $list_fields;
 
-// select columns for table display
 $sql = prepare_sql_tab($list_fields);
 $sql['SQL']  .= "FROM $table_name WHERE (hardware_id = $systemid)";
 
